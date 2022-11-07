fad8c9f777af5f6f77d39dae02a19f55dc131b69
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2022-11-07 09:30:57 +0200

    Final Edit

--- Casino Game/start_game_func.py
+++ Casino Game/start_game_func.py
@@ -20,6 +20,7 @@ def start_game():
     wager_amount = int(input("Enter wager amount above R10 please!: "))
 
     while wager_amount != 0:
+        
         user_prediction = int(input("Guess a number between 0,100"))
         comp_guess = random.randint(0,100)
         
@@ -30,7 +31,7 @@ def start_game():
             wager_amount -= 10
             print("Oops you just lost R10, The number was: ",comp_guess,"Try again!")
             print("You have: R",wager_amount,"left")
-            new_amount = (update(casino_acc).where(casino_acc.c.account_number == acc_number)).values(balance=casino_acc.c.balance - wager_amount)
+            new_amount = (update(casino_acc).where(casino_acc.c.account_number == acc_number)).values(balance=casino_acc.c.balance - 10)
             engine.execute(new_amount)
         exit = input("continue Y/n?")
         if exit == "Y":
